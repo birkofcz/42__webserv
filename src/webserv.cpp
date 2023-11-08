59e7f1dfc6912b33f6dbe2c2b57967ccd66ef1e0
Stepan Benes <sbenes@c3r1s3.42prague.com>
2023-11-08 16:59:07 +0100

    initial commit

---
+++ src/webserv.cpp
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   webserv.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sbenes <sbenes@student.42prague.com>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/08 11:11:06 by sbenes            #+#    #+#             */
+/*   Updated: 2023/11/08 11:18:53 by sbenes           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int main(int /* argc */, char **/* argv */)
+{
+	std::cout << std::endl << "This is where the webserv will hopefully be in a few weeks :) !" << std::endl << std::endl;
+	return (0);
+}
