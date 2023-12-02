7d83c4555793234ac01461e0222256e72e3976f3
Tomas Kajanek <tkajanek@c3r2s4.42prague.com>
2023-12-02 20:21:55 +0100

    messy manager with initialization of servers and accepting clients

--- src/webserv.cpp
+++ src/webserv.cpp
@@ -6,24 +6,28 @@
 /*   By: tkajanek <tkajanek@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/08 11:11:06 by sbenes            #+#    #+#             */
-/*   Updated: 2023/12/06 17:09:18 by tkajanek         ###   ########.fr       */
+/*   Updated: 2023/12/06 17:10:13 by tkajanek         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/general.hpp"
 #include "../include/Parser.hpp"
 #include "../include/Server.hpp"
+#include <iostream>
+#include <cstdlib>
 
 int main(int argc, char **argv)
 {
 	if (argc != 2)
 	{
 		print("Usage: ./webserv <config_file>", RED, 2);
-		return 1;
+		exit(EXIT_FAILURE);
 	}
 	Parser parser(argv[1]);
 	parser.parseFile(argv[1]);
 	parser.printServers();
-	
+
+	manager.setupServers(cluster.getServers());
+	manager.runServers();
 	return 0;
 }
