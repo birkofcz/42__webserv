fa1afd9f9612f28ea1f13bcc1b7c930210f5276b
Stepan Benes <sbenes@c2r6s6.42prague.com>
2023-12-03 16:24:27 +0100

    basic Parser and Server class done

--- src/webserv.cpp
+++ src/webserv.cpp
@@ -6,27 +6,24 @@
 /*   By: sbenes <sbenes@student.42prague.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/08 11:11:06 by sbenes            #+#    #+#             */
-/*   Updated: 2023/11/27 15:52:44 by sbenes           ###   ########.fr       */
+/*   Updated: 2023/12/03 15:45:27 by sbenes           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/general.hpp"
+#include "../include/Parser.hpp"
+#include "../include/Server.hpp"
 
 int main(int argc, char **argv)
 {
-/* 	string message = "This is where the webserv will hopefully be in a few weeks :) !";
-	
-	cout << endl;
-	print(message, GREEN);
-	print(message, RED, 2);
-	print(message);
-	print(message, NONE, 2);
-	cout << endl; */
 	if (argc != 2)
 	{
 		print("Usage: ./webserv <config_file>", RED, 2);
 		return 1;
 	}
-	parseFile(argv[1]);
+	Parser parser(argv[1]);
+	parser.parseFile(argv[1]);
+	parser.printServers();
+	
 	return 0;
 }
